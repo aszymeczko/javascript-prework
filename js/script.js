fb1a7ee7c26bceca283a0e76c59feee209c4816e
Agata Szymeczko <agata.szymeczko21@gmail.com>
2024-09-25 18:30:13 +0200

    Add player move

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-jeśli (losowa liczba == '1') {
+if (randomNumber == '1') {
   computerMove = 'kamień';
-} w przeciwnym razie jeśli (losowa liczba == '2') {
-  komputerMove = 'papier';
-} w przeciwnym razie jeśli (losowa liczba == '3') {
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
   computerMove = 'nożyce';
-} w przeciwnym razie {
+} else {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+  printMessage('Błędny wybór');
+}
+printMessage('Twój ruch: ' + playerMove);
