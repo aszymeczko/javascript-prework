94b0b30fdf125e65a417cc2ee425337ddcc4f166
Agata Szymeczko <agata.szymeczko21@gmail.com>
2024-09-25 18:06:23 +0200

    Add computer move randomization

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var ruchkomputera, ruchgracza;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + ' Wygrałem!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+jeśli (losowa liczba == '1') {
+  computerMove = 'kamień';
+} w przeciwnym razie jeśli (losowa liczba == '2') {
+  komputerMove = 'papier';
+} w przeciwnym razie jeśli (losowa liczba == '3') {
+  computerMove = 'nożyce';
+} w przeciwnym razie {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
