f941b9b706b707a55615e7fddae8214d5aeacc87
Agata Szymeczko <agata.szymeczko21@gmail.com>
2024-10-02 20:16:38 +0200

    Change var to const for moves

--- js/script.js
+++ js/script.js
@@ -9,8 +9,6 @@ function buttonClicked(argButtonName){
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
 
-    var argComputerMove, argMoveId, argPlayerMove, computerMove, playerMove, randomNumber;
-
     /**
      * Describe this function...
      */
@@ -52,13 +50,11 @@ function buttonClicked(argButtonName){
       updateResult(playerResult, computerResult)
     }
 
-    playerMove = argButtonName;
-    console.log('wybór ruchu gracza to: ' + playerMove);
-    playerMove = argButtonName;
+    const playerMove = argButtonName;
     console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
 }
